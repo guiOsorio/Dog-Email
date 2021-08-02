f4226d9daab03ecb58d191270dcd4dc3c95cbfe2
guiOsorio <gosorio@sandiego.edu>
2021-08-02 16:22:49 +0100

    Added lambda function and package

--- send_dog.py
+++ send_dog.py
@@ -10,15 +10,15 @@ dog_image_url = dog_info['url']
 
 EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
 EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-EMAIL_TO = os.environ.get('EMAIL_TO')
+MAIL_TO = os.environ.get('MAIL_TO')
 
 msg = EmailMessage()
 msg['Subject'] = 'Your dog of the day!'
 msg['From'] = EMAIL_ADDRESS
-msg['To'] = [EMAIL_TO]
+msg['To'] = [MAIL_TO]
 
 msg.set_content(f"Have a great day :)!\n{dog_image_url}")
-msg.add_alternative(f'Have a great day :)<br> Here is a dog image to cheer you up<br><img src="{dog_image_url}" width="300px">', subtype='html')
+msg.add_alternative(f'Here is a dog image to cheer you up<br><br><img src="{dog_image_url}" width="300px"><br><br>Have a great day :)', subtype='html')
 
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
     smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
